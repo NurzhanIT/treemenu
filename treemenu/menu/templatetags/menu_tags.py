681967e11b4b800be944b84037c6d6adf1f6d14d
Nurzhan <mrb.nurzhan@gmail.com>
2023-04-12 03:48:14 +0600

    added initial menu data into migratins file, and changed url check method

--- treemenu/menu/templatetags/menu_tags.py
+++ treemenu/menu/templatetags/menu_tags.py
@@ -1,6 +1,7 @@
 import re
 from ..models import MenuListItem
 from django import template
+from django.urls import reverse, NoReverseMatch
 
 register = template.Library()
 
@@ -9,27 +10,34 @@ register = template.Library()
 def draw_menu(context, name, parent=0):
     menu_items = MenuListItem.objects.filter(menu__name=name).select_related('parent')
     if parent != 0 and 'menu' in context:
-        print(context['menu'])
         menu_items_data = context['menu']
     else:
         current_path = context['request'].path
         menu_items_data = []
         check_path = re.compile(r'^http[s]?://')
         for menu_item in menu_items:
+            path = menu_item.path
             parent_id = menu_item.parent.id if menu_item.parent else 0
-            print(parent_id)
-            print(menu_item.pk, 'id')
-            is_active = True if menu_item.path == current_path and current_path else False
-            is_url = True if check_path.match(menu_item.path) else False
+            is_active = True if path == current_path else False
+
+            if check_path.match(path):
+                is_url = True
+                url = path
+            else:
+                is_url = False
+                try:
+                    url = reverse(path)
+                except NoReverseMatch:
+                    url = path
+
             menu_items_data.append({
                 'id': menu_item.pk,
-                'path': menu_item.path,
-                'current_path': current_path,
                 'is_active': is_active,
                 'parent': parent_id,
                 'menu_item_name': menu_item.name,
                 'name': name,
-                'is_url':is_url,
+                'is_url': is_url,
+                'url': url,
             })
 
     return {'menu': menu_items_data,
